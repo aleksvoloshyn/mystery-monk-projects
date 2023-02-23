3c81bb55f56c506c4c53f1c91f1afdcb07aebf1d
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2023-02-23 22:30:45 +0200

    job datails markup added

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const editButton = document.querySelectorAll('.table-editimage__image')
+const editButton = document.querySelectorAll('.edit-image')
 const boardTable = document.querySelector('.board-table')
+const project = document.querySelector('.project')
+const searchBar = document.querySelector('.search-bar')
 const detailsBackButton = document.querySelector('.board-details__button--back')
 const boardDetailsPage = document.querySelector('.board-details')
 
 editButton.forEach((i) => {
   i.addEventListener('click', () => {
+    console.log(1)
     boardTable.classList.toggle('hide')
     boardDetailsPage.classList.toggle('hide')
+    project.classList.toggle('hide')
+    searchBar.classList.toggle('hide')
   })
 })
 
