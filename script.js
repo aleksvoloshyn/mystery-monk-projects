4256dcf87f47206d1b39094a9c3bc6fe72eec119
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2023-02-24 20:54:24 +0200

    markup

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const boardTable = document.querySelector('.board-table')
 const boardContent = document.querySelector('.board-content')
 const projects = document.querySelector('.projects')
 const searchBar = document.querySelector('.search-bar')
+const searchBarMobile = document.querySelector('.search-bar--sm')
 const detailsBackButton = document.querySelector('.board-details__button--back')
 const boardDetailsPage = document.querySelector('.board-details')
 
 editButton.forEach((i) => {
   i.addEventListener('click', () => {
-    console.log(1)
     boardTable.classList.toggle('hide')
     boardDetailsPage.classList.toggle('hide')
     projects.classList.toggle('hide')
-    searchBar.style.display = 'none'
     boardContent.style.display = 'block'
+    searchBarMobile.style.display = 'none'
   })
 })
 
@@ -21,6 +21,11 @@ detailsBackButton.addEventListener('click', () => {
   boardTable.classList.toggle('hide')
   boardDetailsPage.classList.toggle('hide')
   projects.classList.toggle('hide')
-  searchBar.style.display = 'flex'
-  // searchBar.style.display = 'flex'
+  searchBarMobile.style.display = 'flex'
+
+  if (screen.width < 1440) {
+    searchBarMobile.style.display = 'flex'
+  } else {
+    searchBarMobile.style.display = 'none'
+  }
 })
